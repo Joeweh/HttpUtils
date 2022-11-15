4589eeb2632acd0d403a8bf98fa4b54adf28e9f5
Joseph Siracusa <joeyqsa@outlook.com>
2022-11-15 05:48:46 -0500

    Initial commit

---
+++ src/main/java/org/github/joeweh/http/HttpMethod.java
@@ -0,0 +1,4 @@
+package org.github.joeweh.http;
+
+public class HttpMethod {
+}
---
+++ src/main/java/org/github/joeweh/http/Main.java
@@ -0,0 +1,7 @@
+package org.github.joeweh.http;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
